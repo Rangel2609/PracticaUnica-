a10ce110af5044d5f753faa1c48980e345a43160
Rangel2609 <69400987+Rangel2609@users.noreply.github.com>
2020-08-25 02:08:15 -0600

    Version 2

--- Practica/Codigo.py
+++ Practica/Codigo.py
@@ -1,7 +1,7 @@
 print("Bienvenido")
 input("Presione entrer para continuar")
 def opciomes():
-    print("0")
+    print("")
 print("1.Cargar")
 print("2.Seleccionar")
 print("3.Maximo")
@@ -10,11 +10,36 @@ print("5.Suma")
 print("6.Cuenta")
 print("7.Reportar")
 
-seleccion == input("Ingresa el numero de la opcion que desees seleccionar")
-if seleccion== "1":
-    print("Usted a seleccionado Cargar")
-    pass
-else: seleccion== "2":
-    pass print("Usted a selcccionado Seleccionar")
 
 
+while True:
+	opciomes()
+ 
+	seleccion = input("Ingresa el numero a tu seleccion:"+ "")
+ 
+	if seleccion=="1":
+		print ("Usted a seleccionado Cargar")
+		break
+	elif seleccion=="2":
+		print ("Usted a seleccionado Seleccionar")
+		break
+	   
+	elif seleccion=="3":
+		print ("Usted a seleccionado Maximo")
+		break
+	elif seleccion=="4":
+		print("Usted a seleccionado Minimo")
+		break
+	elif seleccion=="5":
+		print("Usted a seleccionado Suma")
+		break
+	elif seleccion=="6":
+		print("Usted a seleccionado Cuenta")
+		break
+	elif seleccion=="7":
+		print("Usted a seleccionado Reprotar")
+		break
+	else:
+		print("Seleccione una opcione que sea valida")
+		
+		
